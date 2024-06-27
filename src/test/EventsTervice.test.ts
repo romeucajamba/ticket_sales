e99526ed4f5ab4b4424f875865fd879e85ea6c63
romeucajamba <romeucajamba@gmail.com>
2024-06-27 16:14:53 +0100

    tested the ticket creationt funcionallity

--- src/test/EventsTervice.test.ts
+++ src/test/EventsTervice.test.ts
@@ -1,8 +1,13 @@
 import { describe, expect, test } from "vitest";
 
+//Create or register attendee
 import { AttendeesRepositoryPrisma } from '../repositories/attendeeRepository.js';
 import { RegisterAttendeeUseCase } from '../controllers/useCases/registerAttendee.js';
 import { UserAllradyExistError } from '../error/emailExistError.js';
+
+// Insert or register ticket
+import { InsertTicketUseCase } from '../controllers/useCases/getTicketAttendee.js';
+import { TicketRepository } from '../repositories/insertTicket.js';
 
 
 
@@ -22,7 +27,7 @@ describe("Register Attendee", async () => {
       
   });
 
-  test("should be regect register the same email", async () => {
+  test.skip("should be regect register the same email", async () => {
     //Teste de integração, testando como as partes da minha aplicação se comportam integradas com outras
     
     const prismaRepository = new AttendeesRepositoryPrisma()
@@ -47,4 +52,20 @@ describe("Register Attendee", async () => {
     ).rejects.toBeInstanceOf(UserAllradyExistError)
 
   });
+
+  test("should be able to insert ticket", async () =>{
+
+    const ticketPrismaRepository = new TicketRepository()
+    const insert_ticketUseCase = new InsertTicketUseCase(ticketPrismaRepository)
+
+    const idAttendee = 'd9d02f72-32a6-4332-9f82-741492147eac';
+    const maxQuantity = 20
+
+    const createTicket = await insert_ticketUseCase.createTicket({
+      idAttendee,
+      maxQuantity
+    })
+
+    return createTicket
+  })
 });
